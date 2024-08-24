14d3726c1655157e82b66fe3e003912411f8ec5f
Rockoder <rfuenzalidao@gmail.com>
2024-08-24 16:56:39 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+
+    // Remover la clase "active" de la imagen actual
+    items[currentIndex].classList.remove('active');
+
+    // Cambiar el índice según la dirección
+    currentIndex += direction;
+
+    // Asegurarse de que el índice esté dentro de los límites
+    if (currentIndex >= totalItems) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = totalItems - 1;
+    }
+
+    // Añadir la clase "active" a la nueva imagen
+    items[currentIndex].classList.add('active');
+}
